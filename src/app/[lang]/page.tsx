c5959e2a4e1854ac908f72f29b18d7f85c720918
Rodrigorjc <rjaencobos@safareyes.es>
2025-04-08 05:57:53 +0200

    ✨ Testeo navbar.

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useDictionary } from "@context";
+import Navbar from "../components/ui/Navbar";
+
 export default function Home() {
     const { dict } = useDictionary();
 
@@ -8,8 +10,12 @@ export default function Home() {
     }
 
     return (
-        <button>
-            {dict.HOME.WELCOME}
-        </button>
+        <div>
+            <Navbar dict={dict}></Navbar>
+            <button>
+                {dict.HOME.WELCOME}
+            </button>
+        </div>
+
     );
 }
