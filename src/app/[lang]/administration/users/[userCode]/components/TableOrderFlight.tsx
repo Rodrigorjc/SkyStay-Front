9227ded9db6a975a4c4ebf5d1e8bf075cb1f3a43
Antonio Nogues <anogues23@gmail.com>
2025-04-25 19:55:10 +0200

    ✨ Mejorar la presentación de las tablas de pedidos de vuelos y hoteles, ajustando estilos y clases para una mejor legibilidad.

--- src/app/[lang]/administration/users/[userCode]/components/TableOrderFlight.tsx
+++ src/app/[lang]/administration/users/[userCode]/components/TableOrderFlight.tsx
@@ -18,12 +18,12 @@ export default function TableOrderFlight({ data }: TableOrderFlightProps) {
         <div className="overflow-auto">
           <table className="table-auto w-full border-separate border-spacing-0 border border-gray-300 rounded-xl overflow-hidden text-sm">
             <thead>
-              <tr className="text-left">
+              <tr className="text-bold text-justify text-base">
                 <th className="border border-gray-300 px-4 py-2 bg-glacier-600 w-1/2">{dict.ADMINISTRATION.USERS.DETAILS.CODE}</th>
                 <th className="border border-gray-300 px-4 py-2 bg-glacier-600 w-1/2">{dict.ADMINISTRATION.USERS.DETAILS.ORDER_CODE}</th>
                 <th className="border border-gray-300 px-4 py-2 bg-glacier-600 w-1/2">{dict.ADMINISTRATION.USERS.DETAILS.AMOUNT}</th>
                 <th className="border border-gray-300 px-4 py-2 bg-glacier-600 w-1/2">{dict.ADMINISTRATION.USERS.DETAILS.DISCOUNT}</th>
-                <th className="border border-gray-300 px-4 py-2 bg-glacier-600 w-1/2">{dict.ADMINISTRATION.USERS.DETAILS.STATUS}</th>
+                <th className="border border-gray-300 px-4 py-2 bg-glacier-600 w-1/2">{dict.ADMINISTRATION.STATUS}</th>
                 <th className="border border-gray-300 px-4 py-2 bg-glacier-600 w-1/2">{dict.ADMINISTRATION.USERS.DETAILS.BILL}</th>
               </tr>
             </thead>
