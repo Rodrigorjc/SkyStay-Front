e26cc55718813ab7a551c19cfea43616acabbf3d
Antonio Nogues <anogues23@gmail.com>
2025-05-25 13:22:30 +0200

    ✨ Formatear código en Notifications.tsx: mejorar la legibilidad del código al ajustar la indentación y el espaciado.

--- src/app/interfaces/Notifications.tsx
+++ src/app/interfaces/Notifications.tsx
@@ -1,10 +1,10 @@
-export interface Notifications{
-    titulo: string;
-    mensaje: string;
-    code: number;
-    tipo?: string;
+export interface Notifications {
+  titulo: string;
+  mensaje: string;
+  code: number;
+  tipo?: string;
 }
 
 export function isNotification(error: unknown): error is Notifications {
-    return (error as Notifications).titulo !== undefined;
+  return (error as Notifications).titulo !== undefined;
 }
