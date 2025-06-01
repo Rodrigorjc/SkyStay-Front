dfaabef60f071f982e3b4c724db5532ea0c4044b
Rodrigorjc <rjaencobos@safareyes.es>
2025-06-01 17:03:10 +0200

    ✨ Refactor accommodation page to integrate Navbar and improve localization handling

--- src/app/[lang]/accommodation/page.tsx
+++ src/app/[lang]/accommodation/page.tsx
@@ -1,35 +1,26 @@
 'use client';
 import AccommodationSearchBar from "./components/AccommodationSearchBar";
 import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import Navbar from "@components/Navbar";
+import {useDictionary} from "@context";
 
-interface SearchFilters {
-    // Define aquí las propiedades de tus filtros, por ejemplo:
-    location?: string;
-    checkIn?: Date;
-    checkOut?: Date;
-    guests?: number;
-}
 
 const AccommodationPage = () => {
     const router = useRouter();
-
-    const handleSearch = (filters: SearchFilters) => {
-        // En Next.js usamos el router para navegar
-        // Puedes usar query params para pasar los filtros
-        const params = new URLSearchParams();
-
-        // Agrega los filtros a los parámetros de URL
-        Object.entries(filters).forEach(([key, value]) => {
-            if (value) params.append(key, String(value));
-        });
-
-        router.push(`list?${params.toString()}`);
-    };
+    const pathname = usePathname();
+    const lang = pathname.split("/")[1] || "en";
+    const { dict } = useDictionary();
+    if (!dict || Object.keys(dict).length === 0) {
+        return null;
+    }
 
     return (
         <div>
-            <h1 className="text-2xl font-bold mb-4">Encuentra tu alojamiento ideal</h1>
-            <AccommodationSearchBar onSearch={handleSearch} />
+            <div className="bg-glacier-300 py-0.5">
+                <Navbar dict={dict}></Navbar>
+            </div>
+            <AccommodationSearchBar onSearch={() => {}} />
         </div>
     );
 };
