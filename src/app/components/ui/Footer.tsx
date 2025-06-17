54170869c9904898d41f0611c65531514310a02d
Antonio Nogues <anogues23@gmail.com>
2025-06-17 21:58:41 +0200

    🔨 Añadir página Legal con aviso legal, términos de servicio, política de privacidad y política de cookies; actualizar enlaces en el footer para soportar múltiples idiomas.

--- src/app/components/ui/Footer.tsx
+++ src/app/components/ui/Footer.tsx
@@ -2,13 +2,13 @@
 import React from "react";
 import { FaXTwitter, FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa6";
 import Image from "next/image";
-import { useDictionary } from "@/app/context/DictionaryContext";
+import { useDictionary, useLanguage } from "@/app/context/DictionaryContext";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
+  const lang = useLanguage();
   const { dict } = useDictionary();
-  if (!dict) {
-    return null;
-  }
+
   const socialLinks = [
     { icon: <FaXTwitter size={20} />, href: "https://x.com" },
     { icon: <FaInstagram size={20} />, href: "https://instagram.com" },
@@ -23,11 +23,15 @@ const Footer: React.FC = () => {
   ];
 
   const legalLinks = [
-    { name: dict.CLIENT.FOOTER.PRIVACY_POLICY, href: "/privacidad" },
-    { name: dict.CLIENT.FOOTER.COOKIES_POLICY, href: "/cookies" },
-    { name: dict.CLIENT.FOOTER.TERMS_OF_SERVICE, href: "/terminos" },
-    { name: dict.CLIENT.FOOTER.LEGAL_NOTICE, href: "/aviso-legal" },
+    { name: dict.CLIENT.FOOTER.PRIVACY_POLICY, href: `/${lang}/legal` },
+    { name: dict.CLIENT.FOOTER.COOKIES_POLICY, href: `/${lang}/legal` },
+    { name: dict.CLIENT.FOOTER.TERMS_OF_SERVICE, href: `/${lang}/legal` },
+    { name: dict.CLIENT.FOOTER.LEGAL_NOTICE, href: `/${lang}/legal` },
   ];
+
+  if (!dict) {
+    return null;
+  }
 
   return (
     <footer className="w-full bg-glacier-500/50 backdrop-blur-md border-t border-white/20 text-white mt-10 pt-10 px-6">
@@ -68,9 +72,9 @@ const Footer: React.FC = () => {
           <ul className="space-y-2 text-sm">
             {legalLinks.map((link, i) => (
               <li key={i}>
-                <a href={link.href} className="hover:underline hover:text-black transition">
+                <Link href={link.href} className="hover:underline hover:text-black transition">
                   {link.name}
-                </a>
+                </Link>
               </li>
             ))}
           </ul>
